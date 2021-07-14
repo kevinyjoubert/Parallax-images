a7fc8fc2b39df298a1821462c2c32d44e5a3e61d
kevinyjoubert <kevinytaka@gmail.com>
2021-07-14 00:27:28 -0300

    add scrollreveal but broken the page - try to fix

---
+++ script/main-scrollreveal.js
@@ -0,0 +1,46 @@
+// reset all element inside page
+
+    ScrollReveal().reveal(
+        `   .box-center,
+            .box-right,
+            .box-left,
+            #three h2,
+            #three p`,
+
+        {   reset:true,
+            origin: 'top',
+            distance: '150px',
+            duration: 900,
+            rotate: {
+                z: 7
+            },
+            scale: 1.5,
+            delay: 500      }
+);
+
+
+// reset gallery images - left
+
+    ScrollReveal().reveal(
+        '   .transition-right',
+
+        {   reset:true,
+            origin: 'left',
+            distance: '100px',
+            duration: 3000,
+            delay: 500      }
+);
+
+
+// reset gallery images - right
+
+    ScrollReveal().reveal(
+        '   .transition-left',
+
+        {   
+            reset:true,
+            origin: 'right',
+            distance: '100px',
+            duration: 3000,
+            delay: 500      }
+);
